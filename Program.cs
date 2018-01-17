db56646717c80f5334ada1862b0559f06f920c8c
flydge <followflydge@gmail.com>
2018-01-17 20:37:51 +0300

    Develop custom class TextTransformer which transforms input text into uppercase text and converts null strings into something valuable. (using iterator)

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringClassTask
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = "Sample Text, Sample Text, Sample all the way";
+            Console.WriteLine(new TextTransformer().TransformToUpper(text));
+        }
+    }
+}
---
+++ TextTransformer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StringClassTask
+{
+    public class TextTransformer
+    {
+        public string TransformToUpper(String text)
+        {
+            string outputText;
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                string[] splitedText = text.Split(' ');
+                StringBuilder stringBuilder = new StringBuilder(String.Empty);
+                foreach (var word in splitedText)
+                {
+                    stringBuilder.Append((word + "||").ToUpper());
+                }
+                outputText = stringBuilder.ToString();
+            }
+            else
+            {
+                outputText = "String is empty"; 
+            }
+            
+            return outputText;
+        }
+    }
+}
